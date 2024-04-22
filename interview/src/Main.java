05e76ef8b6e9ece398dae2472f720d6a806be093
Adrian <ovalle27@gmail.com>
2024-04-22 16:21:58 -0600

    Finished code for interview with corrections.
    This version has mainly two modification: 
    I. Regarding the array order: 
            I reverted the order of the arrays items due to two factors:
            1. My better understanding, after the interview, on how roman numbers are created.
              1.1 According to Wikipedia**: "A number containing two or more decimal digits is built
                  by appending the Roman numeral equivalent for each, from HIGHEST TO LOWEST."
                     Example : 246 = CC + XL + VI = CCXLVI
                                      789 = DCC + LXXX + IX = DCCLXXXIX
             2. Based on point (1), makes more sense for efficiency to go from HIGHEST TO LOWEST.
    II. Regarding the order and number of items on my arrays; they were improved and matched between them, since it was needed on the while logic to be aligned on their value and cell Ex. M = 1000, CM = 900, etc. Without this the logic on line 19 and 21 would make no sense. 
    
    
    REFERENCES: 
    ** Wikipedia reference: https://en.wikipedia.org/wiki/Roman_numerals

--- interview/src/Main.java
+++ interview/src/Main.java
@@ -1,43 +1,43 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static String  DecimalToRoman (int  num){
-        int [] decimalNumber = {1, 4, 5, 9, 10, 40, 90};
-        String[] romanValues = {"I", "IV", "V","IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM"};
+    public static String DecimalToRoman(int num) {
+        // I reverted the order of the arrays items due to two factors:
+        // 1. My better understanding, after the interview, on how roman numbers are created.
+        //  1.1 According to Wikipedia: "A number containing two or more decimal digits is built
+        //      by appending the Roman numeral equivalent for each, from HIGHEST TO LOWEST."
+        //         Example : 246 = CC + XL + VI = CCXLVI
+        // 2. Based on point (1), makes more sense for efficiency to go from HIGHEST TO LOWEST.
+        int[] decimalNumber = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        // Both arrays must have their equivalent value on the same cell position: 1000 = M, 900 = CM, etc.
+        String[] romanValues = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
         StringBuilder roman = new StringBuilder();
-        // 3 = iii
-        for(int i = 0 ; i < decimalNumber.length ; i++){
-            3 bigger or equal than 1 : yes   (grap I)    I
-            3 bigger or equa than 2 : yes   (grab I)    II
-            3 bigger or equal than 3 : yes  (grab I)    III
 
-            3 bigger or equal than 4 : no  ...STOP!
-
+        for (int i = 0; i < decimalNumber.length; i++) {
+            while (num >= decimalNumber[i]) {
+                // Append the corresponding roman value.
+                roman.append(romanValues[i]);
+                //subtracting to get the remain decimal amount.
+                num -= decimalNumber[i];
+            }
         }
-
-     return "";
+        return roman.toString();
     }
 
     public static void main(String[] args) {
-        String finalRoman
+        String finalRoman;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter an integer: ");
         int decimal = scanner.nextInt();
         scanner.close();
-
-        if(decimal >= 1 || decimal <= 3000){
-            //create
-
+        // Change logic of if-statement, since it was always true with previous.
+        if (decimal > 0 && decimal <= 3000) {
+            finalRoman = DecimalToRoman(decimal);
+            System.out.println(finalRoman);
         }
-        else{
-            System.out.println("Wrong input. Try again");
-        }
-
-
-
-
+        // Now, this else is reachable after fixing logic at 'if'.
+        else {
+            System.out.println("Invalid input");
         }
     }
 }
